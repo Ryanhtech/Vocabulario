d99f3dd272f5a7fc2724810501719004fd8c8341
Ryanhtech Labs <70380949+Ryanhtech@users.noreply.github.com>
2022-01-13 17:47:49 +0100

    Reverted update

--- app/src/main/java/com/ryanhtech/vocabulario/tools/collection/CollectionWord.kt
+++ app/src/main/java/com/ryanhtech/vocabulario/tools/collection/CollectionWord.kt
@@ -1,10 +1,17 @@
 /*
  * Copyright 2021-2022 Ryanhtech Labs
  *
- * This Software (Vocabulario) is licensed under the RHT-PSLA
- * license (Ryanhtech Proprietary Software License Agreement).
- * You must not use this Software or this source code if you
- * don't comply with this license.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package com.ryanhtech.vocabulario.tools.collection
